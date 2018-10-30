1f463df368993e5af4b46d02b8494cfe03d165cd
Jason Chiu <jason.chiu@inmobi.com>
2018-10-30 10:52:03 -0700

    Add placeholder 'template' and additional pages, hook it up to the nav

--- nav.js
+++ nav.js
@@ -18,10 +18,10 @@ var nav = new Vue(
   el: '#navigation',
   data: {
     navList: [
-      { id: 0, text: 'About', url: '#', isActive: false },
-      { id: 1, text: 'Projects', url: '#', isActive: false },
-      { id: 2, text: 'Resume', url: '#', isActive: false },
-      { id: 3, text: 'Contact', url: '#', isActive: false }
+      { id: 0, text: 'About', url: 'about.html', isActive: false },
+      { id: 1, text: 'My Work', url: 'mywork.html', isActive: false },
+      { id: 2, text: 'Resume', url: 'http://jasonthechiu.com/resume/resume.pdf', isActive: false },
+      { id: 3, text: 'Contact', url: 'contact.html', isActive: false }
     ]
   }
 }
